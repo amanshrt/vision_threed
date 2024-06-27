2b530d9e60ce449461e3d95de4333e032990e8bd
Subash Shrestha <119420796+arconsis-subash@users.noreply.github.com>
2024-06-27 09:18:06 +0200

    Initialization

---
+++ 3d_vision_test/Landmarks/Views/GlobeModule.swift
@@ -0,0 +1,18 @@
+//
+//  GlobeModule.swift
+//  3d_vision_test
+//
+//  Created by Subash Shrestha on 27.06.24.
+//
+
+import SwiftUI
+
+struct GlobeModule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GlobeModule()
+}
---
+++ 3d_vision_test/Landmarks/Views/LandmarkList/LandmarkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetailView.swift
+//  3d_vision_test
+//
+//  Created by Subash Shrestha on 23.06.24.
+//
+
+import SwiftUI
+
+struct LandmarkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetailView()
+}
